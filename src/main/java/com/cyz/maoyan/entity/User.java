4569deb4ad3b40b0411f64b2d91c42661703dfeb
cyz <1904406675@qq.com>
2025-08-08 12:10:55 +0800

    登录 注册 个人信息更新(日期有问题  日期问题解决)

--- src/main/java/com/cyz/maoyan/entity/User.java
+++ src/main/java/com/cyz/maoyan/entity/User.java
@@ -1,5 +1,6 @@
 package com.cyz.maoyan.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,6 +25,7 @@ public class User {
     private String username;
     private String nickName;
     private String sex;
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date birthday;
     private String livingState;
     private String industry;
